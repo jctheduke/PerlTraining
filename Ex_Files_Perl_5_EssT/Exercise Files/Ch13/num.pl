13bab7365afbe6a9f6900af17d0c011a24d8ba03
jctheduke <priyaranjan780@gmail.com>
2017-01-18 13:55:56 -0700

    feat : Built in String and Numeric functions
    different string functions and Numeric functions are discussed.

--- Ex_Files_Perl_5_EssT/Exercise Files/Ch13/num.pl
+++ Ex_Files_Perl_5_EssT/Exercise Files/Ch13/num.pl
@@ -10,3 +10,34 @@ my $b = 150;
 my $x = $a + $b;
 
 say "result is: $x";
+
+# absolute value abs
+
+say abs($a -$b);
+
+# atan2 -- inverse of tan
+# calcuating the value of pie.
+
+say atan2(1,1)*4;
+
+# square root
+
+say sqrt($a);
+
+# power op
+say 2 **4;
+
+# oct and hexadecimal numbers bases
+
+say hex 'f';
+
+# int will floor the number
+say int 7.9999;
+
+# random number
+
+say rand;
+
+# seed randnumber
+my $sr = srand(14);
+say $sr;
--- Ex_Files_Perl_5_EssT/Exercise Files/Ch13/string.pl
+++ Ex_Files_Perl_5_EssT/Exercise Files/Ch13/string.pl
@@ -6,3 +6,40 @@ use warnings;
 
 my $string = "This string has a bunch of useful characters in it.";
 say $string;
+
+# legth of string
+say length($string);
+
+# chomp will remove line endings
+say length(chomp($string));
+
+# chop is not used its old functions.It doesnt check well
+chop $string;
+say $string;
+
+# substring is to get or alter a portion of the string
+say substr $string,5,10;
+
+# replaceing
+
+substr($string,5,10,'changed');
+say $string;
+
+# index return the index of the string search
+say index $string,'in',3;
+# here 3 is the search string position.
+
+
+# reverse will reverse the string
+say scalar reverse($string);
+
+# lc - lower case
+reverse($string);
+say lc $string;
+
+# uc -- upper case
+say uc $string;
+
+# ucfirst - upper case of first letter of sentence.
+say ucfirst $string;
+
